23fd55bfe437ebfd63b0e5e50aa7620e7fdb934c
Florent Cayré <florent@commown.coop>
2025-09-19 19:06:34 +0200

    Remove pygount hardcoded version and improve compatibility
    pygount 1.4.0 is now very old and current code does not work on recent versions.
    It is however easy to improve compatibility accross pygount versions using the
    code_count property instead of the code one, which was removed in 2.0.0.
    That way, pygount 1.4.0 stays supported but also modern version up to at least
     3.1.0.

--- module_analysis/__manifest__.py
+++ module_analysis/__manifest__.py
@@ -26,7 +26,7 @@
         "data/ir_module_type_rule.xml",
     ],
     "external_dependencies": {
-        "python": ["pygount==1.4.0"],
+        "python": ["pygount"],
     },
     "installable": True,
 }
